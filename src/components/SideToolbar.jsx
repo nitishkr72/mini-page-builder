f130f94193591bac249c3974af7268da3f6e14b3
Nitish Kumar <nks77811@gmail.com>
2023-12-21 17:11:23 +0530

    added export functionality

--- src/components/SideToolbar.jsx
+++ src/components/SideToolbar.jsx
@@ -6,6 +6,24 @@ import { BlocksContext } from "../context/BlocksContext";
 export default function SideToolbar() {
   const { blocksData, setBlocksData } = useContext(BlocksContext);
 
+  function handleExport() {
+    const jsonData = JSON.stringify(blocksData.blocks);
+
+    const url = URL.createObjectURL(
+      new Blob([jsonData], { type: "application/json" })
+    );
+
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "mini-page-builder.json";
+    document.body.appendChild(link);
+    link.click();
+
+    document.body.removeChild(link);
+
+    URL.revokeObjectURL(url);
+  }
+
   return (
     <div className="fixed top-0 right-0 z-10 h-auto md:w-60 xl:w-80 xl:h-full bg-[#2D2D2D] px-6 py-5">
       <span className="text-white font-bold text-xl">BLOCKS</span>
@@ -22,15 +40,21 @@ export default function SideToolbar() {
           );
         })}
       </div>
-      <div>
+      <div className="grid grid-cols-1 xl:grid-cols-2 grid-rows-2 xl:grid-rows-1 gap-3 mt-4">
         <button
-          className="text-white text-base bg-[#0044C1] px-4 py-2 mt-4 rounded-sm"
+          className="text-white text-base bg-[#0044C1] px-4 py-2  rounded-sm"
           onClick={() => {
             localStorage.removeItem("blocks");
             setBlocksData({ ...blocksData, blocks: [] });
           }}
         >
           Clear Screen
+        </button>
+        <button
+          className="text-white text-base bg-[#0044C1] px-4 py-2 rounded-sm"
+          onClick={handleExport}
+        >
+          Export
         </button>
       </div>
     </div>
